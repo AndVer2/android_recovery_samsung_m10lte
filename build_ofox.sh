ec4a022e17f2ff9a9b7c11cd2630f591bcff352c
youssefnone <113144168+youssefnone@users.noreply.github.com>
2022-09-14 19:31:37 +0100

    Update build_ofox.sh

--- build_ofox.sh
+++ build_ofox.sh
@@ -1,1 +1,9 @@
+export OF_VANILLA_BUILD=1
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export OF_USE_TWRP_SAR_DETECT=1
+export OF_FLASHLIGHT_ENABLE=0
+export OF_QUICK_BACKUP_LIST="/boot;/data;/vendor_image;/system_image;"
+
+
+
 
